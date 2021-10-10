387b0af583ec4ad1922046d5215b8947d4493f1a
Aravind Chandran <aravind.chandran@speridian.com>
2021-10-10 11:22:30 +0530

    Login Page

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -6,10 +6,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  active = 1;
+
+  activeClass = "text-dark fw-bold linkText linkTextActive"
+  inactiveClass = "text-dark fw-bold linkText"
 
   constructor() { }
 
   ngOnInit(): void {
   }
+
+  changeAuthAdmin(){
+    this.active = 1;
+  }
+
+  changeAuthDonor(){
+    this.active = 2; 
+  }
+
+  changeAuthBloodBank(){
+    this.active = 3;
+  }
+
 
 }
